3b9f28ee62cd01eb5b463853f45947bbe8beeca2
yanchao <yanchao563@yahoo.com>
2017-03-09 22:51:04 +0800

    新增核心函数库

--- src/Koiterm/Inc/CommonFunc.php
+++ src/Koiterm/Inc/CommonFunc.php
@@ -10,7 +10,15 @@
  * @link      yanchao563@yahoo.com
  */
 
+/**
+ * 尽量不要在此文件中添加全局函数
+ * 请在source/class/helper/目录下创建相应的静态函数集类文件
+ * 类的静态方法可以在产品中所有地方使用，使用方法类似：helper_form::submitcheck()
+ */
+
+
 namespace Koiterm\Inc;
+use Koiterm\Base\KoError;
 use Koiterm\Init\Core as C;
 
 class CommonFunc
@@ -148,6 +156,296 @@ class CommonFunc
         }
         return false;
     }
+
+    /**
+     * 检查邮箱是否有效
+     * @param $email 要检查的邮箱
+     * @param 返回结果
+     */
+    public static function isemail($email) {
+        return strlen($email) > 6 && strlen($email) <= 32 && preg_match("/^([A-Za-z0-9\-_.+]+)@([A-Za-z0-9\-]+[.][A-Za-z0-9\-.]+)$/", $email);
+    }
+
+    /**
+     * 产生随机码
+     * @param $length - 要多长
+     * @param $numberic - 数字还是字符串
+     * @return 返回字符串
+     */
+    function random($length, $numeric = 0) {
+        $seed = base_convert(md5(microtime().$_SERVER['DOCUMENT_ROOT']), 16, $numeric ? 10 : 35);
+        $seed = $numeric ? (str_replace('0', '', $seed).'012340567890') : ($seed.'zZ'.strtoupper($seed));
+        if($numeric) {
+            $hash = '';
+        } else {
+            $hash = chr(rand(1, 26) + rand(0, 1) * 32 + 64);
+            $length--;
+        }
+        $max = strlen($seed) - 1;
+        for($i = 0; $i < $length; $i++) {
+            $hash .= $seed{mt_rand(0, $max)};
+        }
+        return $hash;
+    }
+
+    /**
+     * 判断一个字符串是否在另一个字符串中存在
+     *
+     * @param string 原始字串 $string
+     * @param string 查找 $find
+     * @return boolean
+     */
+    function strexists($string, $find) {
+        return !(strpos($string, $find) === FALSE);
+    }
+
+    /**
+    得到时间戳
+     */
+    function dmktime($date) {
+        if(strpos($date, '-')) {
+            $time = explode('-', $date);
+            return mktime(0, 0, 0, $time[1], $time[2], $time[0]);
+        }
+        return 0;
+    }
+
+    /**
+     * 针对uft-8进行特殊处理的strlen
+     * @param string $str
+     * @return int
+     */
+    function dstrlen($str) {
+        if(strtolower(CHARSET) != 'utf-8') {
+            return strlen($str);
+        }
+        $count = 0;
+        for($i = 0; $i < strlen($str); $i++){
+            $value = ord($str[$i]);
+            if($value > 127) {
+                $count++;
+                if($value >= 192 && $value <= 223) $i++;
+                elseif($value >= 224 && $value <= 239) $i = $i + 2;
+                elseif($value >= 240 && $value <= 247) $i = $i + 3;
+            }
+            $count++;
+        }
+        return $count;
+    }
+
+    /**
+     * 根据中文裁减字符串
+     * @param $string - 字符串
+     * @param $length - 长度
+     * @param $doc - 缩略后缀
+     * @return 返回带省略号被裁减好的字符串
+     */
+    public static function cutstr($string, $length, $dot = ' ...') {
+        if(strlen($string) <= $length) {
+            return $string;
+        }
+
+        $pre = chr(1);
+        $end = chr(1);
+        $string = str_replace(array('&amp;', '&quot;', '&lt;', '&gt;'), array($pre.'&'.$end, $pre.'"'.$end, $pre.'<'.$end, $pre.'>'.$end), $string);
+
+        $strcut = '';
+        if(strtolower(CHARSET) == 'utf-8') {
+
+            $n = $tn = $noc = 0;
+            while($n < strlen($string)) {
+
+                $t = ord($string[$n]);
+                if($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
+                    $tn = 1; $n++; $noc++;
+                } elseif(194 <= $t && $t <= 223) {
+                    $tn = 2; $n += 2; $noc += 2;
+                } elseif(224 <= $t && $t <= 239) {
+                    $tn = 3; $n += 3; $noc += 2;
+                } elseif(240 <= $t && $t <= 247) {
+                    $tn = 4; $n += 4; $noc += 2;
+                } elseif(248 <= $t && $t <= 251) {
+                    $tn = 5; $n += 5; $noc += 2;
+                } elseif($t == 252 || $t == 253) {
+                    $tn = 6; $n += 6; $noc += 2;
+                } else {
+                    $n++;
+                }
+
+                if($noc >= $length) {
+                    break;
+                }
+
+            }
+            if($noc > $length) {
+                $n -= $tn;
+            }
+
+            $strcut = substr($string, 0, $n);
+
+        } else {
+            $_length = $length - 1;
+            for($i = 0; $i < $length; $i++) {
+                if(ord($string[$i]) <= 127) {
+                    $strcut .= $string[$i];
+                } else if($i < $_length) {
+                    $strcut .= $string[$i].$string[++$i];
+                }
+            }
+        }
+
+        $strcut = str_replace(array($pre.'&'.$end, $pre.'"'.$end, $pre.'<'.$end, $pre.'>'.$end), array('&amp;', '&quot;', '&lt;', '&gt;'), $strcut);
+
+        $pos = strrpos($strcut, chr(1));
+        if($pos !== false) {
+            $strcut = substr($strcut,0,$pos);
+        }
+        return $strcut.$dot;
+    }
+
+    /**
+     * 分页
+     * @param $num - 总数
+     * @param $perpage - 每页数
+     * @param $curpage - 当前页
+     * @param $mpurl - 跳转的路径
+     * @param $maxpages - 允许显示的最大页数
+     * @param $page - 最多显示多少页码
+     * @param $autogoto - 最后一页，自动跳转
+     * @param $simple - 是否简洁模式（简洁模式不显示上一页、下一页和页码跳转）
+     * @return 返回分页代码
+     */
+    public static function multi($num, $perpage, $curpage, $mpurl, $maxpages = 0, $page = 10, $autogoto = FALSE, $simple = FALSE, $jsfunc = FALSE) {
+        return $num > $perpage ? \Koiterm\Base\HelperPage::multi($num, $perpage, $curpage, $mpurl, $maxpages, $page, $autogoto, $simple, $jsfunc) : '';
+    }
+
+    /**
+     * 只有上一页下一页的分页（无需知道数据总数）
+     * @param $num - 本次所取数据条数
+     * @param $perpage - 每页数
+     * @param $curpage - 当前页
+     * @param $mpurl - 跳转的路径
+     * @return 返回分页代码
+     */
+    public static function simplepage($num, $perpage, $curpage, $mpurl) {
+        return \Koiterm\Base\HelperPage::simplepage($num, $perpage, $curpage, $mpurl);
+    }
+
+    /*
+    * 递归创建目录
+    */
+    public static function dmkdir($dir, $mode = 0777, $makeindex = TRUE){
+        if(!is_dir($dir)) {
+            dmkdir(dirname($dir), $mode, $makeindex);
+            @mkdir($dir, $mode);
+            if(!empty($makeindex)) {
+                @touch($dir.'/index.html'); @chmod($dir.'/index.html', 0777);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 刷新重定向
+     */
+    function dreferer($default = '') {
+        global $_G;
+
+        $default = empty($default) && $_ENV['curapp'] ? $_ENV['curapp'].'.php' : '';
+        $_G['referer'] = !empty($_GET['referer']) ? $_GET['referer'] : $_SERVER['HTTP_REFERER'];
+        $_G['referer'] = substr($_G['referer'], -1) == '?' ? substr($_G['referer'], 0, -1) : $_G['referer'];
+
+
+        $reurl = parse_url($_G['referer']);
+
+        if(!$reurl || (isset($reurl['scheme']) && !in_array(strtolower($reurl['scheme']), array('http', 'https')))) {
+            $_G['referer'] = '';
+        }
+
+        if(!empty($reurl['host']) && !in_array($reurl['host'], array($_SERVER['HTTP_HOST'], 'www.'.$_SERVER['HTTP_HOST'])) && !in_array($_SERVER['HTTP_HOST'], array($reurl['host'], 'www.'.$reurl['host']))) {
+            if(!in_array($reurl['host'], $_G['setting']['domain']['app']) && !isset($_G['setting']['domain']['list'][$reurl['host']])) {
+                $domainroot = substr($reurl['host'], strpos($reurl['host'], '.')+1);
+            }
+        } elseif(empty($reurl['host'])) {
+            $_G['referer'] = $_G['siteurl'].'./'.$_G['referer'];
+        }
+
+        $_G['referer'] = durlencode($_G['referer']);
+        return $_G['referer'];
+    }
+
+    /**
+     * url序列化
+     * @param $url
+     * @return mixed
+     */
+    public static function durlencode($url) {
+        static $fix = array('%21', '%2A','%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
+        static $replacements = array('!', '*', ';', ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
+        return str_replace($fix, $replacements, urlencode($url));
+    }
+
+
+    /**
+     * 重建数组
+     * @param <string> $array 需要反转的数组
+     * @return array 原数组与的反转后的数组
+     */
+    function renum($array) {
+        $newnums = $nums = array();
+        foreach ($array as $id => $num) {
+            $newnums[$num][] = $id;
+            $nums[$num] = $num;
+        }
+        return array($nums, $newnums);
+    }
+
+    /**
+     * 字节格式化单位
+     * @param $filesize - 大小(字节)
+     * @return 返回格式化后的文本
+     */
+    function sizecount($size) {
+        if($size >= 1073741824) {
+            $size = round($size / 1073741824 * 100) / 100 . ' GB';
+        } elseif($size >= 1048576) {
+            $size = round($size / 1048576 * 100) / 100 . ' MB';
+        } elseif($size >= 1024) {
+            $size = round($size / 1024 * 100) / 100 . ' KB';
+        } else {
+            $size = intval($size) . ' Bytes';
+        }
+        return $size;
+    }
+
+
+
+    /**
+     * 安全的 intval， 可以支持 int(10) unsigned
+     * 支持最大整数 0xFFFFFFFF 4294967295
+     * @param mixed $int string|int|array
+     * @return mixed
+     */
+    function dintval($int, $allowarray = false) {
+        $ret = intval($int);
+        if($int == $ret || !$allowarray && is_array($int)) return $ret;
+        if($allowarray && is_array($int)) {
+            foreach($int as &$v) {
+                $v = dintval($v, true);
+            }
+            return $int;
+        } elseif($int <= 0xffffffff) {
+            $l = strlen($int);
+            $m = substr($int, 0, 1) == '-' ? 1 : 0;
+            if(($l - $m) === strspn($int,'0987654321', $m)) {
+                return $int;
+            }
+        }
+        return $ret;
+    }
+
+
+
     /**
      * 去掉slassh
      * @param $string
@@ -245,6 +543,59 @@ class CommonFunc
         return true;
     }
 
+    /**
+     * 设置cookie
+     * @param $var - 变量名
+     * @param $value - 变量值
+     * @param $life - 生命期
+     * @param $prefix - 前缀
+     */
+    public static function dsetcookie($var, $value = '', $life = 0, $prefix = 1, $httponly = false)
+    {
+        global $_G;
+
+        $config = $_G['config']['cookie'];
+
+        $_G['cookie'][$var] = $value;
+        $var = ($prefix ? $config['cookiepre'] : '').$var;
+        $_COOKIE[$var] = $value;
+
+        if($value == '' || $life < 0) {
+            $value = '';
+            $life = -1;
+        }
+
+        if(defined('IN_MOBILE')) {
+            $httponly = false;
+        }
+
+        $life = $life > 0 ? getglobal('timestamp') + $life : ($life < 0 ? getglobal('timestamp') - 31536000 : 0);
+        $path = $httponly && PHP_VERSION < '5.2.0' ? $config['cookiepath'].'; HttpOnly' : $config['cookiepath'];
+
+        $secure = $_SERVER['SERVER_PORT'] == 443 ? 1 : 0;
+        if(PHP_VERSION < '5.2.0') {
+            setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure);
+        } else {
+            setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure, $httponly);
+        }
+    }
+
+    /**
+     * 获取cookie
+     */
+
+    public static function getcookie($key) {
+        global $_G;
+        return isset($_G['cookie'][$key]) ? $_G['cookie'][$key] : '';
+    }
+
+    /**
+     * 获取文件扩展名
+     */
+    public static function fileext($filename) {
+        return addslashes(strtolower(substr(strrchr($filename, '.'), 1, 10)));
+    }
+
 
     public static function dheader($string, $replace = true, $http_response_code = 0) {
         $islocation = substr(strtolower(trim($string)), 0, 8) == 'location';
@@ -327,5 +678,4 @@ class CommonFunc
         $hashadd = defined('IN_ADMINCP') ? 'Only For Ashu! Admin YanChao' : '';
         return substr(md5(substr($_G['timestamp'], 0, -7).$_G['username'].$_G['uid'].$_G['authkey'].$hashadd.$specialadd), 8, 8);
     }
-
 }
